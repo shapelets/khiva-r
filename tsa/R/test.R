99c1e642cb2ee5a09038d5abe9a6291439d0505f
David Cuesta <david.cuesta@shapelets.io>
2018-03-07 10:22:44 +0100

    License and Copyright added

--- tsa/R/test.R
+++ tsa/R/test.R
@@ -1,3 +1,9 @@
+#Copyright (c) 2018 Grumpy Cat Software S.L.
+#
+#This Source Code Form is subject to the terms of the Mozilla Public
+#License, v. 2.0. If a copy of the MPL was not distributed with this
+#file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
 ta <- as.double(c(1, 2, 3, 5, 6, 7, 8, 10))
 tb <- as.double(c(4, 5, 6, 24, 24, 24, 3, 3))
 library(bit64)
