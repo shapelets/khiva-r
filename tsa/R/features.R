71902f3108002b70c1acb166cf9b1812ca12cead
David Cuesta <david.cuesta@shapelets.io>
2018-03-14 19:52:19 +0000

    R library reorganization

---
+++ tsa/R/features.R
@@ -0,0 +1,135 @@
+#
+#Copyright (c) 2018 Grumpy Cat Software S.L.
+#
+#This Source Code Form is subject to the terms of the Mozilla Public
+#License, v. 2.0. If a copy of the MPL was not distributed with this
+#file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+#' @brief Calculates de sum over the square values of the time series.
+#' 
+#' @param  time.series List of time series double arrays.
+#' @return List with the Absolute Energy. 
+#' @export 
+
+AbsEnergy <- function(time.series) {
+  
+  shared.library <-system.file("extdata","libTSALIB.dylib",package="tsa")
+  dyn.load(shared.library,PACKAGE='libTSALIB.dylib')
+  
+  library("bit64")
+  
+  time.series.length <- as.integer64(length(time.series[[1]]))
+  concatenated.time.series <- as.double(apply( cbind( time.series ), 1 ,unlist))
+  number.of.time.series <- as.integer64(length(time.series))
+  
+  try(
+    out <- .C("abs_energy",
+              concatenated.time.series,
+              time.series.length,
+              number.of.time.series,
+              result = as.double(seq(length = number.of.time.series), from = 0, to = 0),
+              PACKAGE='libTSALIB.dylib')
+  )
+  
+  newList <- list("result" = out$result)
+  
+  return(newList)
+}
+
+#' @brief Calculates the sum over the absolute value of consecutive 
+#' changes in the time series
+#' 
+#' @param  time.series List of time.series double arrays.
+#' @return List with the absoluteSumOfChanges
+#' @export 
+
+AbsoluteSumOfChanges <- function(time.series) {
+  
+  shared.library <-system.file("extdata","libTSALIB.dylib",package="tsa")
+  dyn.load(shared.library,PACKAGE='libTSALIB.dylib')
+  
+  library("bit64")
+  
+  time.series.length <- as.integer64(length(time.series[[1]]))
+  concatenated.time.series <- as.double(apply( cbind( time.series ), 1 ,unlist))
+  number.of.time.series <- as.integer64(length(time.series))
+  
+  try(
+    out <- .C("absolute_sum_of_changes",
+              concatenated.time.series,
+              time.series.length,
+              number.of.time.series,
+              result = as.double(seq(length = number.of.time.series), from = 0, to = 0),
+              PACKAGE='libTSALIB.dylib')
+  )
+  
+  newList <- list("result" = out$result)
+  
+  return(newList)
+}
+
+#' @brief c3 function.
+#' 
+#' @param time.series List of time series double arrays.
+#' @param lag The lag.
+#' @return The non-linearity value for the given time series.
+#' @export 
+
+C3 <- function(tss, lag) {
+  
+  shared.library <- system.file("extdata", "libTSALIB.dylib", package = "tsa")
+  dyn.load(shared.library, PACKAGE = 'libTSALIB.dylib')
+  
+  library("bit64")
+  
+  tss.length <- as.integer64(length(tss[[1]]))
+  tss.concatenated <- as.double(apply(cbind(tss), 1, unlist))
+  tss.number.of.ts <- as.integer64(length(tss))
+  
+  try(
+    out <- .C("c3",
+              tss.concatenated,
+              tss.length,
+              tss.number.of.ts,
+              as.integer64(lag),
+              result = as.double(seq(length = tss.number.of.ts), from = 0, to = 0),
+              PACKAGE='libTSALIB.dylib')
+  )
+  
+  r.result <- list("result" = out$result)
+  
+  return(r.result)
+}
+
+#' @brief cidCe function.
+#' 
+#' @param tss List of time series double arrays.
+#' @param z.normalize Controls whether the time series should be z-normalized or not.
+#' @return The complexity value for the given time series.
+#' @export 
+
+CidCe <- function(tss, z.normalize) {
+  
+  shared.library <- system.file("extdata", "libTSALIB.dylib", package="tsa")
+  dyn.load(shared.library, PACKAGE = 'libTSALIB.dylib')
+  
+  library("bit64")
+  
+  tss.length <- as.integer64(length(tss[[1]]))
+  tss.concatenated <- as.double(apply(cbind(tss), 1, unlist))
+  tss.number.of.ts <- as.integer64(length(tss))
+  
+  try(
+    out <- .C("cidCe",
+              tss.concatenated,
+              tss.length,
+              tss.number.of.ts,
+              z.normalize,
+              result = as.double(seq(length = tss.number.of.ts), from = 0, to = 0),
+              PACKAGE='libTSALIB.dylib')
+  )
+  
+  r.result <- list("result" = out$result)
+  
+  return(r.result)
+}
---
+++ tsa/R/matrix.R
@@ -0,0 +1,148 @@
+#
+#Copyright (c) 2018 Grumpy Cat Software S.L.
+#
+#This Source Code Form is subject to the terms of the Mozilla Public
+#License, v. 2.0. If a copy of the MPL was not distributed with this
+#file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+#'Stomp algorithm
+#'
+#' @brief'STOMP algorithm to calculate the matrix profile between 'ta' and 'tb' using a subsequence length
+#'of 'm'.
+#' 
+#' @param firs.time.series The first time series.
+#' @param second.time.series The second time series.
+#' @param subsequence.lenth Length of the subsequence.
+#' @return A matrix profile 
+#' @export 
+
+Stomp <- function(first.time.series, second.time.series, subsequence.length) {
+  
+  shared.library <-system.file("extdata","libTSALIB.dylib",package="tsa")
+  dyn.load(shared.library,PACKAGE='libTSALIB.dylib')
+  
+  library("bit64")
+  
+  try(
+    out <- .C("stomp",
+              as.double(first.time.series),
+              as.double(second.time.series),
+              as.integer(length(first.time.series)),
+              as.integer(length(second.time.series)),
+              as.integer64(subsequence.length),
+              p = as.double(seq(length = (length(second.time.series) - subsequence.length + 1), from = 0, to = 0)),
+              i = as.integer(seq(length = (length(second.time.series) - subsequence.length + 1), from = 0, to = 0)),
+              PACKAGE='libTSALIB.dylib')
+  )
+  
+  newList <- list("profile" = out$p, "index" = out$i)
+  
+  return(newList)
+}
+
+#' Stomp self join algorithm
+#' @brief 'STOMP algorithm to calculate the matrix profile between 't' and itself using a subsequence length
+#'of 'm'. This method filters the trivial matches.
+#' 
+#' @param first.time.series The query and reference time series.
+#' @param subsequence.length Lenght of the subsequence
+#' @return A matrix profile 
+#' @export
+
+StompSelfJoin <- function(first.time.series,subsequence.length) {
+  
+  shared.library <-system.file("extdata","libTSALIB.dylib",package="tsa")
+  
+  dyn.load(shared.library)
+  
+  library("bit64")
+  
+  try(
+    out <- .C("stomp_self_join",
+              as.double(first.time.series),
+              as.integer(length(first.time.series)),
+              as.integer64(subsequence.length),
+              p = as.double(seq(length=(length(first.time.series) - subsequence.length + 1), from = 0, to = 0)),
+              i = as.integer(seq(length=(length(first.time.series) - subsequence.length + 1), from = 0, to = 0)),
+              PACKAGE='libTSALIB.dylib')
+  )
+  
+  newList <- list("profile" = out$p, "index" = out$i)
+  
+  return(newList)
+}
+
+#' find_best_n_discords.R
+#' @brief Primitive of the findBestNDiscords function.
+#
+#' @param profile The matrix profile containing the minimum distance of each
+#' subsequence.
+#' @param index The matrix profile index containing where each minimum occurs.
+#' @param n Number of motifs to extract.
+#' @return A list with the discor distances, the discord indices and the subsequences indices.
+#' @export
+
+FindBestNDiscords <- function(profile,index,n) {
+  
+  shared.library <-system.file("extdata","libTSALIB.dylib",package="tsa")
+  
+  dyn.load(shared.library)
+  
+  library("bit64")
+  
+  try(
+    out <- .C("find_best_n_discords",
+              as.double(profile),
+              as.integer(index),
+              as.integer64(length(profile)),
+              as.integer64(n),
+              discord.distance = as.double(seq(length=n, from = 0, to = 0)),
+              discord.index = as.integer(seq(length=n, from = 0, to = 0)),
+              subsequence.index = as.integer(seq(length=n, from = 0, to = 0)),
+              PACKAGE='libTSALIB.dylib')
+  )
+  
+  newList <- list("discord.distance" = out$discord.distance,
+                  "discord.index" = out$discord.index,
+                  "subsequence.index" = out$subsequence.index)
+  
+  return(newList)
+}
+
+#' find_best_n_motifs.R
+#' @brief Primitive of the findBestNMotifs function.
+#
+#' @param profile The matrix profile containing the minimum distance of each
+#' subsequence
+#' @param index The matrix profile index containing where each minimum occurs
+#' @param length_profile Length of the matrix profile
+#' @param n Number of motifs to extract
+#' @return A list with the motif distance, the motif indices and the subsequence indices.
+#' @export
+
+FindBestNMotifs <- function(profile,index,n) {
+  
+  shared.library <-system.file("extdata","libTSALIB.dylib",package="tsa")
+  
+  dyn.load(shared.library)
+  
+  library("bit64")
+  
+  try(
+    out <- .C("find_best_n_motifs",
+              as.double(profile),
+              as.integer(index),
+              as.integer64(length(profile)),
+              as.integer64(n),
+              motif.distance = as.double(seq(length=n, from = 0, to = 0)),
+              motif.index = as.integer(seq(length=n, from = 0, to = 0)),
+              subsequence.index = as.integer(seq(length=n, from = 0, to = 0)),
+              PACKAGE='libTSALIB.dylib')
+  )
+  
+  newList <- list("motif.distance" = out$motif.distance,
+                  "motif.index" = out$motif.index,
+                  "subsequence.index" = out$subsequence.index)
+  
+  return(newList)
+}
