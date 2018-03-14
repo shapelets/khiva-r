5613f5a565bdec20d87dfe0cf5dd2940106684e8
David Cuesta <david.cuesta@shapelets.io>
2018-03-14 17:56:44 +0000

    cidCe and c3 functions added.

--- tsa/R/test.R
+++ tsa/R/test.R
@@ -4,13 +4,15 @@
 #License, v. 2.0. If a copy of the MPL was not distributed with this
 #file, You can obtain one at http://mozilla.org/MPL/2.0/.
 #
-ta <- as.double(c(1, 2, 3, 5, 6, 7, 8, 10))
-tb <- as.double(c(4, 5, 6, 24, 24, 24, 3, 3))
+ta <- as.double(c(0, 1, 2, 3, 4, 5))
+tb <- as.double(c(6, 7, 8, 9, 10, 11))
 library(bit64)
 m  <- as.integer64(3)
-out <- Stomp(ta,tb,m)
-out1 <- StompSelfJoin(tb,m)
-out2 <- FindBestNMotifs(out$profile, out$index, 5)
-out3 <- FindBestNDiscords(out$profile, out$index, 5)
-out4 <- AbsoluteSumOfChanges(list(ta,tb))
-out5 <- AbsEnergy((list(ta,tb)))
+out <- Stomp(ta, tb, m)
+out1 <- StompSelfJoin(tb, m)
+out2 <- FindBestNMotifs(out$profile, out$index, 3)
+out3 <- FindBestNDiscords(out$profile, out$index, 3)
+out4 <- AbsoluteSumOfChanges(list(ta, tb))
+out5 <- AbsEnergy((list(ta, tb)))
+out6 <- C3((list(ta, tb)), 2)
+out7 <- CidCe((list(ta, tb)), FALSE)
