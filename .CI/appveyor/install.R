cf2e3da9f5dd04dd0f91830458f88daca83c77cf
Oscar Torreno <oscar.torreno@shapelets.io>
2018-06-19 16:52:59 +0200

    First try with the AppVeyor CI

---
+++ .CI/appveyor/install.R
@@ -0,0 +1,14 @@
+# Copyright (c) 2018 Shapelets.io
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+library(devtools)
+install.packages("bit64")
+install.packages("testthat")
+library(testthat)
+install.packages("roxygen2")
+library(roxygen2)
+library(methods)
+save.image(file='CIsession.RData')
