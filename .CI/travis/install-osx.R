87f1ce97cd79ba6e0481d39fb5973283e46c04ff
David Cuesta <david.cuesta@shapelets.io>
2018-06-21 18:07:13 +0200

    blank line added

--- .CI/travis/install-osx.R
+++ .CI/travis/install-osx.R
@@ -3,6 +3,7 @@
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 install.packages("devtools", repos="https://cloud.r-project.org/")
 library(devtools)
 install.packages("bit64", repos="https://cloud.r-project.org/")
