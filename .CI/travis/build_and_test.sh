0531488f5acecf2a71dbc9bff21739e590bb5c13
Oscar Torreno <oscar.torreno@shapelets.io>
2018-06-25 11:24:21 +0200

    Run the coverage in all the operating systems (#12)
    * Run the coverage in all the operating systems
    
    * AppVeyor modifications
    
    * Fix install.sh travis
    
    * Specify XML file to be uploaded in AppVeyor

--- .CI/travis/build_and_test.sh
+++ .CI/travis/build_and_test.sh
@@ -5,8 +5,4 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
-    Rscript .CI/travis/script-osx.R
-else
-    Rscript .CI/travis/script-linux.R
-fi
+Rscript .CI/travis/script.R
