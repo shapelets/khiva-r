1e86dcdd72d8b824511933770c6d76ada6e7e79f
Oscar Torreno <oscar.torreno@shapelets.io>
2018-06-29 12:11:22 +0200

    Fix in copyright header

--- demo/khiva.R
+++ demo/khiva.R
@@ -1,9 +1,8 @@
+# Copyright (c) 2018 Shapelets.io
 #
-#Copyright (c) 2018 Shapelets.io
-#
-#This Source Code Form is subject to the terms of the Mozilla Public
-#License, v. 2.0. If a copy of the MPL was not distributed with this
-#file, You can obtain one at http://mozilla.org/MPL/2.0/.
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 # Creation of a khiva Array
 ta <- as.double(c(1, 2, 3, 4, 5, 6, 7, 8))
