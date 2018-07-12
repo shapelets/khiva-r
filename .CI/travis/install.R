d99912d10f623e7dc1ea9fbdde61d325c7973a64
Antonio Vilches <antonio.vilches@shapelets.io>
2018-07-12 12:49:16 +0200

    Change appveyor to find Rscript path

--- .CI/travis/install.R
+++ .CI/travis/install.R
@@ -4,11 +4,11 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-list.of.packages <- c("devtools", "bit64", "testthat", "roxygen2", "shiny", "DT", "covr", "git2r")
+list.of.packages <- c("devtools", "bit64", "testthat", "roxygen2", "shiny", "DT", "covr")
 new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
 if(length(new.packages)) install.packages(new.packages, repos="https://cloud.r-project.org/")
 
-Packages <- c("devtools", "bit64", "testthat", "roxygen2", "shiny", "DT", "covr", "git2r")
+Packages <- c("devtools", "bit64", "testthat", "roxygen2", "shiny", "DT", "covr")
 lapply(Packages, library, character.only = TRUE)
 
 save.image(file='CIsession.RData')
