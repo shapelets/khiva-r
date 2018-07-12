7861267f851d57754379deea180bf46f17e9b1a7
Antonio Vilches <antonio.vilches@shapelets.io>
2018-07-12 11:49:32 +0200

    Delete library imports

--- tests/testthat/test_library.R
+++ tests/testthat/test_library.R
@@ -5,10 +5,6 @@
 #License, v. 2.0. If a copy of the MPL was not distributed with this
 #file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-library(tidyverse)
-library(httr)
-library(jsonlite)
-
 testthat::setup(
   SetBackend(KHIVABackend()$KHIVA_BACKEND_CPU)
 )
