2408202b7d74a244b6c813ecc4e691017fbf7de4
Antonio Vilches <antonio.vilches@shapelets.io>
2018-07-13 12:08:57 +0200

    Fix spacing

--- .CI/travis/install-khiva.sh
+++ .CI/travis/install-khiva.sh
@@ -41,12 +41,12 @@ else
         cd ..
     fi
 
-     #Installing conan and dependencies
-     if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
+    #Installing conan and dependencies
+    if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
         sudo apt-get update -y
         sudo apt-get install -y python3 python3-pip
         sudo pip3 install conan
-     else
+    else
         brew upgrade pyenv
         export TRAVIS_PYTHON_VERSION=3.6.5
         export PATH=$HOME/.pyenv/shims:$HOME/.pyenv/versions/${TRAVIS_PYTHON_VERSION}/bin:$PATH
@@ -59,8 +59,8 @@ else
         # Installing conan
         sudo pip${PYTHON_VERSION} install --upgrade pip
         sudo pip${PYTHON_VERSION} install conan
-     fi
-     conan remote add -f conan-mpusz https://api.bintray.com/conan/mpusz/conan-mpusz
+    fi
+    conan remote add -f conan-mpusz https://api.bintray.com/conan/mpusz/conan-mpusz
 
     # Cloning Github repo into khiva-library folder
     git clone https://github.com/shapelets/khiva.git ../khiva-library
