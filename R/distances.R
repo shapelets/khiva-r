ee341d3334c8c8ff7325f9bf9a305a528ebc477c
Antonio Vilches <avilchesreina@gmail.com>
2018-12-20 09:56:41 +0100

    Adding sbd method (#24)
    * Adding sbd method
    
    * Not reporting the test results to AppVeyor

--- R/distances.R
+++ R/distances.R
@@ -5,6 +5,46 @@
 #License, v. 2.0. If a copy of the MPL was not distributed with this
 #file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+#' Dtw
+#'
+#' Calculates the Dynamic Time Warping Distance.
+#'
+#' @param tss Expects an input array whose dimension zero is the length of the time series (all the same) and
+#' dimension one indicates the number of time series.
+#' @return KHIVA array with an upper triangular matrix where each position corresponds to the distance between
+#' two time series. Diagonal elements will be zero. For example: Position row 0 column 1 records the
+#' distance between time series 0 and time series 1.
+#' @export
+Dtw <- function(tss) {
+  try(out <- .C("dtw",
+                ptr = tss@ptr,
+                b = as.integer64(0),
+                PACKAGE = package))
+  eval.parent(substitute(tss@ptr <- out$ptr))
+  
+  return(createArray(out$b))
+}
+
+#' Euclidean
+#'
+#' Calculates euclidean distances between time series.
+#'
+#' @param tss Expects an input array whose dimension zero is the length of the time series (all the same) and
+#' dimension one indicates the number of time series.
+#' @return KHIVA array with an upper triangular matrix where each position corresponds to the distance between two
+#' time series. Diagonal elements will be zero. For example: Position row 0 column 1 records the distance
+#' between time series 0 and time series 1.
+#' @export
+Euclidean <- function(tss) {
+  try(out <- .C("euclidean",
+                ptr = tss@ptr,
+                b = as.integer64(0),
+                PACKAGE = package))
+  eval.parent(substitute(tss@ptr <- out$ptr))
+  
+  return(createArray(out$b))
+}
+
 #' Euclidean
 #'
 #' Calculates euclidean distances between time series.
@@ -65,18 +105,19 @@ Manhattan <- function(tss) {
   return(createArray(out$b))
 }
 
-#' Dtw
+#' SBD
 #'
-#' Calculates the Dynamic Time Warping Distance.
+#' Calculates the Shape-Based distance (SBD). It computes the normalized cross-correlation and it returns 1.0 
+#' minus the value that maximizes the correlation value between each pair of time series.
 #'
 #' @param tss Expects an input array whose dimension zero is the length of the time series (all the same) and
 #' dimension one indicates the number of time series.
-#' @return KHIVA array with an upper triangular matrix where each position corresponds to the distance between
-#' two time series. Diagonal elements will be zero. For example: Position row 0 column 1 records the
-#' distance between time series 0 and time series 1.
+#' @return KHIVA array with an upper triangular matrix where each position corresponds to the distance between two
+#' time series. Diagonal elements will be zero. For example: Position row 0 column 1 records the distance
+#' between time series 0 and time series 1.
 #' @export
-Dtw <- function(tss) {
-  try(out <- .C("dtw",
+Sbd <- function(tss) {
+  try(out <- .C("sbd",
                 ptr = tss@ptr,
                 b = as.integer64(0),
                 PACKAGE = package))
--- tests/testthat/test_distances.R
+++ tests/testthat/test_distances.R
@@ -9,6 +9,18 @@ testthat::setup(
   SetBackend(KHIVABackend()$KHIVA_BACKEND_CPU)
 )
 
+context("Khiva Dtw tests")
+
+test_that("Test Dtw", {
+  ta <-
+    as.single(c(1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4, 5, 5, 5, 5, 5))
+  a <- Array(array(ta, dim = c(5, 5)))
+  out <- Dtw(a)
+  b <- c(getData(out))
+  expected = c(0, 0, 0, 0, 0, 5, 0, 0, 0, 0, 10, 5, 0, 0, 0, 15, 10, 5, 0, 0, 20, 15, 10, 5, 0)
+  expect_equal(b, expected, 1e-6)
+})
+
 context("Khiva Euclidean tests")
 
 test_that("Test Euclidean", {
@@ -24,39 +36,6 @@ test_that("Test Euclidean", {
   expect_equal(b[6], 0, 1e-6)
   expect_equal(b[7], 16, 1e-6)
   expect_equal(b[8], 8, 1e-6)
-  expect_equal(b[9], 0, 1e-6)
-
-  deleteArray(a)
-  deleteArray(out)
-})
-
-context("Khiva Dtw tests")
-
-test_that("Test Dtw", {
-  ta <-
-    as.single(c(1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4, 5, 5, 5, 5, 5))
-  a <- Array(array(ta, dim = c(5, 5)))
-  out <- Dtw(a)
-  b <- c(getData(out))
-  expected = c(0, 0, 0, 0, 0, 5, 0, 0, 0, 0, 10, 5, 0, 0, 0, 15, 10, 5, 0, 0, 20, 15, 10, 5, 0)
-  expect_equal(b, expected, 1e-6)
-})
-
-context("Khiva SquaredEuclidean tests")
-
-test_that("Test SquaredEuclidean", {
-  ta <- as.single(c(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11))
-  a <- Array(array(ta, dim = c(4, 3)))
-  out <- SquaredEuclidean(a)
-  b <- c(getData(out))
-  expect_equal(b[1], 0, 1e-6)
-  expect_equal(b[2], 0, 1e-6)
-  expect_equal(b[3], 0, 1e-6)
-  expect_equal(b[4], 64, 1e-6)
-  expect_equal(b[5], 0, 1e-6)
-  expect_equal(b[6], 0, 1e-6)
-  expect_equal(b[7], 256, 1e-6)
-  expect_equal(b[8], 64, 1e-6)
   expect_equal(b[9], 0, 1e-6)
 
   deleteArray(a)
@@ -85,6 +64,48 @@ test_that("Test Manhattan", {
   expected = c(0, 0, 0, 0, 0, 5, 0, 0, 0, 0, 10, 5, 0, 0, 0, 15, 10, 5, 0, 0, 20, 15, 10, 5, 0)
   b <- c(getData(out))
   expect_equal(b, expected)
+  deleteArray(a)
+  deleteArray(out)
+})
+
+context("Khiva SBD tests")
+
+test_that("Test SBD", {
+  ta <- as.single(c(1, 2, 3, 4, 5, 1, 1, 0, 1, 1, 10, 12, 0, 0, 1))
+  a <- Array(array(ta, dim = c(5, 3)))
+  out <- Sbd(a)
+  b <- c(getData(out))
+  expect_equal(b[1], 0, 1e-6)
+  expect_equal(b[2], 0, 1e-6)
+  expect_equal(b[3], 0, 1e-6)
+  expect_equal(b[4], 0.505025, 1e-6)
+  expect_equal(b[5], 0, 1e-6)
+  expect_equal(b[6], 0, 1e-6)
+  expect_equal(b[7], 0.458583, 1e-6)
+  expect_equal(b[8], 0.564093, 1e-6)
+  expect_equal(b[9], 0, 1e-6)
+  
+  deleteArray(a)
+  deleteArray(out)
+})
+
+context("Khiva SquaredEuclidean tests")
+
+test_that("Test SquaredEuclidean", {
+  ta <- as.single(c(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11))
+  a <- Array(array(ta, dim = c(4, 3)))
+  out <- SquaredEuclidean(a)
+  b <- c(getData(out))
+  expect_equal(b[1], 0, 1e-6)
+  expect_equal(b[2], 0, 1e-6)
+  expect_equal(b[3], 0, 1e-6)
+  expect_equal(b[4], 64, 1e-6)
+  expect_equal(b[5], 0, 1e-6)
+  expect_equal(b[6], 0, 1e-6)
+  expect_equal(b[7], 256, 1e-6)
+  expect_equal(b[8], 64, 1e-6)
+  expect_equal(b[9], 0, 1e-6)
+  
   deleteArray(a)
   deleteArray(out)
 })
