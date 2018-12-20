dc3f2507ac78a51b808178341575b2c76603f127
Oscar Torreno <oscar.torreno@shapelets.io>
2018-12-20 08:38:06 +0100

    Back to origin install script

--- .CI/appveyor/install.R
+++ .CI/appveyor/install.R
@@ -4,17 +4,11 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-install.packages("devtools", repos="https://cloud.r-project.org/")
-
-library(devtools)
-
-install_version("covr", version = "3.1.0", repos="https://cloud.r-project.org/")
-
-list.of.packages <- c("bit64", "testthat", "roxygen2", "shiny", "DT")
+list.of.packages <- c("devtools", "bit64", "testthat", "roxygen2", "shiny", "DT", "covr")
 new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
 if(length(new.packages)) install.packages(new.packages, repos="https://cloud.r-project.org/")
 
-Packages <- c("devtools", "usethis", "bit64", "testthat", "roxygen2", "shiny", "DT", "covr")
+Packages <- c("devtools", "bit64", "testthat", "roxygen2", "shiny", "DT", "covr")
 lapply(Packages, library, character.only = TRUE)
 
 save.image(file='CIsession.RData')
