5d3fc4bc3d42d74dc363928e4852f4afac898ffc
David Cuesta <david.cuesta@shapelets.io>
2019-02-27 16:38:08 +0100

    k-shape and k-means added. Its corresponding test has been added too

--- tests/testthat/test_dimensionality.R
+++ tests/testthat/test_dimensionality.R
@@ -6,7 +6,7 @@
 #file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 testthat::setup(
-  SetBackend(KHIVABackend()$KHIVA_BACKEND_CPU)
+  SetBackend(KHIVABackend()$KHIVA_BACKEND_OPENCL)
 )
 
 context("Khiva RamerDouglasPeucker tests")
